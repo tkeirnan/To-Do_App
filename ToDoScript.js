04d3804ed4e54f7016aeb4c90550a0f5aecb05b4
Tommy Keirnan <tkeirnan@gmail.com>
2019-12-21 09:49:12 -0500

    added to code that allows program to remain in the local storage and delete the right pieces of the stored array.

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,44 +1,39 @@
-//retrieve items in local storage => [{ text: '', completed: false }]
-// create all the li elements based on the objects in the array
+// //*ANDYS HELP*
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
 
-function createToDoLiElement(todoItemText){
+function createToDoLiElement(toDoItemText) {
   const todoLi = document.createElement('Li');
   todoLi.className = 'list-group-item';
-  todoLi.appendChild(document.createTextNode(todoItemText))
+  todoLi.appendChild(document.createTextNode(toDoItemText));
 
- //add item to local storage
- //here
- 
   return todoLi;
 }
 
-function createDeleteButton(){
+function createDeleteButton() {
   const deleteBtn = document.createElement('button');
 
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right mt-1 mb-1 delete';
-  
+
   // Append text node to del button
   deleteBtn.appendChild(document.createTextNode('Delete'));
   return deleteBtn;
 }
 
-function createCompletedButton(){
+function createCompletedButton() {
   // Create completed button element
   var completedBtn = document.createElement('button');
 
   // Add classes to completed button
   completedBtn.className = 'btn btn-success btn-sm float-right mt-1 mr-2 mb-1 markCompleted';
-  
+
   // Append text node to completed button
   completedBtn.appendChild(document.createTextNode('Mark Completed'));
   completedBtn.addEventListener('click', strikeItem);
@@ -47,7 +42,7 @@ function createCompletedButton(){
 }
 
 // Add item
-function addItem(e){
+function addItem(e) {
   e.preventDefault();
 
   // Get input value
@@ -55,6 +50,23 @@ function addItem(e){
 
   // Create new li element
   var li = createToDoLiElement(newItem);
+
+  // retrieve items array in local storage and store in variable *ANDYS HELP*;
+  let savedToDos = localStorage.getItem('todo-items');
+  console.log('right after retrieving: ', savedToDos);
+  if (savedToDos === null) {
+    savedToDos = [];
+  } else {
+    savedToDos = JSON.parse(savedToDos);
+  }
+  console.log(savedToDos);
+
+  // add the new todo item information to items array *ANDYS HELP*
+  savedToDos.push(newItem);
+  console.log(savedToDos);
+
+  // update local storage with the new items array ANDYS HELP
+  localStorage.setItem('todo-items', JSON.stringify(savedToDos));
 
   // Create del button element
   var deleteBtn = createDeleteButton();
@@ -64,7 +76,7 @@ function addItem(e){
 
   // Append button to li
   li.appendChild(deleteBtn);
-  
+
   //Append second button to li
   li.appendChild(completedBtn);
 
@@ -76,38 +88,73 @@ function addItem(e){
 }
 
 // Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are you sure you wish to delete?')){
+function removeItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are you sure you wish to delete?")) {
       var li = e.target.parentElement;
       itemList.removeChild(li);
+      //localStorage.removeItem("todo-items"); // delete everything in local storage for that key
+      let savedToDos = localStorage.getItem("todo-items");
+      console.log("right after retrieving: ", savedToDos);
+      if (savedToDos === null) {
+        savedToDos = [];
+      } else {
+        savedToDos = JSON.parse(savedToDos);
+      }
+      console.log(e.target.previousSibling.textContent);
+      
+      var index = savedToDos.indexOf(e.target.previousSibling.textContent);
+      savedToDos.splice(index, 1);
+      console.log(savedToDos);
+      // search for the matching todoitem
+      // remove todoitem from the array
+      // update localstorage with new array
+      localStorage.setItem('todo-items', JSON.stringify(savedToDos));
     }
   }
 }
 
 // Strikethrough item
-function strikeItem(e){
-    if(e.target.classList.contains('markCompleted')){
-        if(confirm('Are you sure you wish to mark as completed?')){
-            var completedBtn = e.currentTarget;
-            completedBtn.parentElement.style.textDecoration = "line-through";
-        }
+function strikeItem(e) {
+  if (e.target.classList.contains('markCompleted')) {
+    if (confirm('Are you sure you wish to mark as completed?')) {
+      var completedBtn = e.currentTarget;
+      completedBtn.parentElement.style.textDecoration = "line-through";
     }
+  }
 }
 
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
+
+let savedToDos = localStorage.getItem('todo-items');
+console.log('right after retrieving: ', savedToDos);
+if (savedToDos === null) {
+  savedToDos = [];
+} else {
+  savedToDos = JSON.parse(savedToDos);
+}
+console.log(savedToDos);
+
+for (let i = 0; i < savedToDos.length; i++) {
+  const toDoText = savedToDos[i];
+
+  //update the DOM
+  //by adding a new li for each toDo
+  // Create new li element
+  var li = createToDoLiElement(toDoText);
+
+  // Create del button element
+  var deleteBtn = createDeleteButton();
+
+  // Create completed button element
+  var completedBtn = createCompletedButton();
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  //Append second button to li
+  li.appendChild(completedBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
 }
